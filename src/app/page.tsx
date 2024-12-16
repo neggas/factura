60126fa215b1f1c82e3a1ee85165a53d0ef08b77
neggas <arsene.fop@gmail.com>
2024-12-16 10:52:24 +0000

    feat(web): endpoind theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 export default function Home() {
   return (
     <Box>
-      <Text textStyle="heading-sm">Building website</Text>
+      <Text textStyle="caption">Building website</Text>
     </Box>
   );
 }
