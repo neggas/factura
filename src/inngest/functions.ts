21b0ae90437ae6902764888424bba5938496006e
neggas <arsene.fop@gmail.com>
2024-12-30 15:31:35 +0000

    reset test key

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -7,7 +7,7 @@ import {
 
 export const ingestNotifyForInvoiceDueToBePaid = inngest.createFunction(
   { id: "notify-for-invoice-due-to-be-paid" },
-  { cron: "TZ=Europe/Paris * * * * *" },
+  { cron: "TZ=Europe/Paris * 3 * * *" },
   async ({ step }) => {
     try {
       const invoices = await getInvoices();
